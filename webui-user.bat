3fa74237f33bde35abd980b1bfd73c80b752a37f
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-09-25 12:13:15 +0800

    fix: API 启动

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --opt-sdp-no-mem-attention --enable-insecure-extension-access
+set COMMANDLINE_ARGS= --api --xformers --opt-sdp-no-mem-attention --enable-insecure-extension-access
 
 call webui.bat
