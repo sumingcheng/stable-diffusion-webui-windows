04b1ce8f10eaec82e0f249f294ad0627e6e7f211
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-03 21:52:49 +0800

    fix: nowebui API

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --api --xformers --opt-sdp-no-mem-attention --enable-insecure-extension-access
+set COMMANDLINE_ARGS= --api --xformers --opt-sdp-no-mem-attention --enable-insecure-extension-access --nowebui
 
 call webui.bat
